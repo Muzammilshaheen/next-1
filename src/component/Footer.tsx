e9251b3a75fbd9d8eef02b416597a0eeb9dd1808
mariaghafoor636 <mariaghafoor636@gmail.com>
2024-09-20 07:32:03 +0500

    next-1

---
+++ src/component/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+
+const Footer = () => {
+  return (
+    <div className="flex bg-red-500 p-3 gap-3 justify-end">
+    <Link href="#" className="pl-3 pr-3">Gmail</Link>
+    <Link href="#" className="pl-3 pr-3">whatsapp</Link>
+    <Link href="#" className="pl-3 pr-3">imo</Link>
+
+    </div>
+     )
+    }
+    export default Footer
